be695c072334c7826d88703200580f9da96f348f
Jitesh Gosar <gosar95@gmail.com>
2024-09-13 01:39:42 +0530

    tests serviceAccountPath

--- middlewares/firebaseAdmin.js
+++ middlewares/firebaseAdmin.js
@@ -1,8 +1,16 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
 
 const serviceAccountPath = process.env.NODE_ENV === 'production'
   ? '/etc/secrets/serviceAccountKey.json'  // Path in Render
   : '../secrets/serviceAccountKey.json';    // Path in local development
+
+if (fs.existsSync(serviceAccountPath)) {
+  console.log('Service account file exists');
+  const serviceAccount = require(serviceAccountPath);
+} else {
+  console.log('Service account file does not exist');
+}
 
 const serviceAccount = require(serviceAccountPath);
 admin.initializeApp({
